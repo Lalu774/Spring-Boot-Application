c99f9ef9bdd841678ddfbf42367bfa7086029bbf
Lalu <Lalu@Lalu>
2018-12-04 16:22:07 +0530

    Controller chages

--- src/main/java/com/employee/controller/EmployeeController.java
+++ src/main/java/com/employee/controller/EmployeeController.java
@@ -20,7 +20,7 @@ import com.employee.service.EmployeeService;
 
 @RestController
 @RequestMapping("api")
-public class EmployeeRestCotroller {
+public class EmployeeController {
 
 	@Autowired
 	private EmployeeService employeeService; 
